9ba90e64a4e8aa0638aa82dad45d96488e6715f3
alfian <alfianwilfredohoris@gmail.com>
2022-11-08 11:34:41 +0700

    fix conditional if email and password empty in regis

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,34 @@ class UserController {
   static async register(req, res) {
     try {
       let { email, password } = req.body;
+      if (!email) {
+        res.status(400).json({ message: "Email can't be empty" });
+      }
+      if (!password) {
+        res.status(400).json({ message: "Password can't be empty" });
+      }
       let regis = await User.create({ email, password });
       res.status(201).json({ id: regis.id, email: regis.email });
     } catch (error) {
       if (error.name === "SequelizeUniqueConstraintError") {
         res.status(400).json({ message: "Email has been registered" });
+      } else {
+        res.status(500).json({ message: "Internal server error" });
       }
+    }
+  }
+  static async login(req, res) {
+    try {
+      let { email, password } = req.body;
+      if (!email) {
+        res.status(400).json({ message: "Email can't be empty" });
+      }
+      if (!password) {
+        res.status(400).json({ message: "Password can't be empty" });
+      }
+      console.log(email, password);
+    } catch (error) {
+      res.status(500).json({ message: "Internal server error" });
     }
   }
 }
--- router/index.js
+++ router/index.js
@@ -3,4 +3,5 @@ const app = express();
 let UserController = require("../controllers/userController");
 
 app.post("/register", UserController.register);
+app.post("/login", UserController.login);
 module.exports = app;
